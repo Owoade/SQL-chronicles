f64bfa8860d2305feb650338fe111deb649940a8
Owoade <owoadeanuoluwapo2@gmail.com>
2025-08-22 02:36:49 +0100

    added CTEs

--- DML/common-table-expressions/non-recursive.sql
+++ DML/common-table-expressions/non-recursive.sql
@@ -11,6 +11,6 @@ AS (SELECT customer_id, "type", created_at FROM activity_log where customer_id =
 SELECT * from customers c INNER JOIN customer_most_recent_activity_log ON customer_most_recent_activity_log.customer_id = c.id WHERE c.id = 1;
 
 -- Example 2
-WITH manager( id, `name` )
-AS (SELECT id, `name` FROM employee WHERE manager_id IS NULL)
+WITH manager( id, "name" )
+AS (SELECT id, "name" FROM employee WHERE manager_id IS NULL)
 SELECT * FROM employee INNER JOIN manager ON employee.manager_id = manager.id WHERE employee.manager_id IS NOT NULL; 
