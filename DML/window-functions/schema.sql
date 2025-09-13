d061a5bf79f707b427bd8849c0331c236c667b03
Owoade <owoadeanuoluwapo2@gmail.com>
2025-09-14 00:32:27 +0100

    added window functions

--- DML/window-functions/schema.sql
+++ DML/window-functions/schema.sql
@@ -5,3 +5,15 @@ CREATE TABLE sales (
     month VARCHAR(10) NOT NULL,    -- month (could also use DATE or INT for order)
     revenue INT NOT NULL           -- revenue for that month
 );
+/*
+Sample Data
+id   employee   month   revenue
+--------------------------------
+1    Ada        Jan     2000
+2    Ada        Feb     2500
+3    Ada        Mar     3000
+4    Bola       Jan     1500
+5    Bola       Feb     1800
+6    Bola       Mar     2200
+7    Femi       Jan     2000
+*/
