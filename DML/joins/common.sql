4b7d85c1c169c5dea2981abaecec40ad15612f5e
Owoade <owoadeanuoluwapo2@gmail.com>
2025-09-23 00:39:50 +0100

    added unions and excluding joins

--- DML/joins/common.sql
+++ DML/joins/common.sql
@@ -20,3 +20,16 @@ SELECT * from table1 RIGHT OUTER JOIN table2 ON table1.col = table2.col;
 
 -- FULL OUTER JOIN(doesn't exist in MySql)
 SELECT * from table1 FULL OUTER JOIN table2 ON table1.col = table2.col; 
+
+-- Excluding join
+SELECT * from left_table LEFT JOIN right_table on left_table.common_id = right_table.common_id WHERE right_table.common_id IS NULL;
+
+-- UNIONS
+/*
+  UNION ALL includes duplicates
+  GROUP BY and HAVING can only be used in each query and cannot affect final result
+  ORDER BY can only be used in the last SELECT statement
+*/
+SELECT name, email, phone_number, "First and Best" as University
+UNION
+SELECT name, email, phone_number, "Others";
