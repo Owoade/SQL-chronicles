4c86898653592ce586dc927ee2e8f050b08df3fb
Owoade <owoadeanuoluwapo2@gmail.com>
2025-10-13 14:28:49 +0100

    added triggers

--- DML/triggers/t.sql
+++ DML/triggers/t.sql
@@ -2,6 +2,6 @@ CREATE TRIGGER log_employee_salary_changes
 AFTER UPDATE ON employee FOR EACH ROW
 BEGIN
     IF OLD.salary != NEW.salary THEN 
-        INSERT INTO employee_salary_log (emp_id, salary) VALUES (OLD.emp_id, OLD.salary);
+        INSERT INTO employee_salary_log (emp_id, salary) VALUES (OLD.id, OLD.salary);
     END IF
 END;
